8d881271c9b2025d75b93c4e0f488b4c79565c42
Usuari@ daw2 <eduardo27fp@gmail.com>
2019-10-23 19:30:17 +0200

    Inicial

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+//index.php
+class App
+{
+    public function __construct()
+    {
+        session_start();
+    }
+    public function tabla()
+    {
+        $apuesta[] = 0;
+        $contador = 1;
+        echo "<table>";
+        for ($i=0; $i < 7 ; $i++) { 
+            echo "<tr>";
+            for ($x=0; $x < 7; $x++) { 
+                if (isset($_SESSION[$contador])) {
+                    $class = 'apuesta';
+                } else {
+
+                    $class = 'noapostado';
+                }
+                echo '<td> <a  href="?method=tabla&number= '.$contador.'" class = "' . $class . '" />  '.$contador.'</td>';
+                
+                $contador ++;
+            }
+            echo "</tr>";
+
+        }
+        echo "</table>";
+       
+        require('vista.php');
+    }
+    public function reiniciar()
+    {
+        session_destroy();
+        unset($_SESSION['apuesta']);
+        header('Location: /Loteria') ;
+        
+        
+    }
+   
+   
+}
+$app = new App();
+if (isset($_GET['method'])) {
+    $method = $_GET['method'];
+} else {
+    $method = 'tabla';
+}
+
+$app->$method();
